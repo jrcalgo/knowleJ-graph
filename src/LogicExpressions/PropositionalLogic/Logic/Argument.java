e8f2bc2f22b2350950ef3cc0a305f037b1b2593b
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-01-01 07:12:40 -0500

    file skeleton updated

--- src/LogicExpressions/PropositionalLogic/Logic/Argument.java
+++ src/LogicExpressions/PropositionalLogic/Logic/Argument.java
@@ -1,15 +1,48 @@
 package src.LogicExpressions.PropositionalLogic.Logic;
 
-import src.Interfaces.Equivalencies;
 import src.LogicExpressions.PropositionalLogic.Laws.InferenceLaws;
 
 public class Argument extends InferenceLaws {
     private Model[] models;
-    private String[] expressions;
-    private 
+    private char[] charEvaluations;
+    private boolean[] booleanEvaluations;
+    private String[][] truthTable;
+    private Boolean[][] valueTable;
 
     public Argument(Model[] models) {
-        this.expression = expression;
+        this.models = models;
+        setTruthTable();
+    }
+    
+    public void checkModels() {
+        
     }
 
+    public boolean evaluateArgument() {
+
+    }
+
+    public setTruthTable() {
+
+    }
+
+    public getTruthTable() {
+
+    }
+
+    public getValueTable() {
+
+    }
+
+    public Model[] getModels() {
+        return this.models;
+    }
+
+    public String[] getArgumentExpressions() {
+        String[] expressions = new String[this.models.length];
+        for (int i = 0; i < this.models.length; i++) {
+            expressions[i] = this.models[i].getExpression();
+        }
+        return expressions;
+    }
 }
