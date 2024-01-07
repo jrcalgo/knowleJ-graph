9216c3c7bc2788be653fb333b59055df8800172b
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-01-07 11:04:43 -0500

    Basic implementation
    Subject to change; created basic evaluation of argument

--- src/LogicExpressions/PropositionalLogic/Logic/Argument.java
+++ src/LogicExpressions/PropositionalLogic/Logic/Argument.java
@@ -4,13 +4,23 @@ import src.LogicExpressions.PropositionalLogic.Laws.InferenceLaws;
 
 public class Argument extends InferenceLaws {
     private Model[] models;
-    private char[] charEvaluations;
-    private boolean[] booleanEvaluations;
-    private String[][] truthTable;
-    private Boolean[][] valueTable;
+    private Model query;
+    private char[] modelCharEvaluations;
+    private boolean[] modelBooleanEvaluations;
+    private char argumentCharEvaluation;
+    private boolean argumentBooleanEvaluation;
+    private String[] truthTable;
+    private Boolean[] tableValues;
 
-    public Argument(Model[] models) {
+    public Argument(Model[] models, Model query) {
         this.models = models;
+        this.query = query;
+        setTruthTable();
+    }
+
+    public Argument(Model[] models, String query) {
+        this.models = models;
+        this.query = new Model("Query", query);
         setTruthTable();
     }
     
@@ -18,20 +28,35 @@ public class Argument extends InferenceLaws {
         
     }
 
+    public void setTruthTable() {
+        this.truthTable = new String[this.models.length + 1];
+        for (int i = 0; i < this.models.length; i++) {
+            this.truthTable[i] = this.models[i].getExpression() + " : " + this.models[i].getPredicateEvaluation();
+        }
+        this.truthTable[this.truthTable.length] = query.getExpression() + " : " + evaluateArgument();
+    }
+
     public boolean evaluateArgument() {
+        this.modelCharEvaluations = new char[this.models.length];
+        this.modelBooleanEvaluations = new boolean[this.models.length];
+        for (int i = 0; i < this.models.length; i++) {
+            this.modelCharEvaluations[i] = this.models[i].getPredicateEvaluation() ? 'T' : 'F';
+            this.modelBooleanEvaluations[i] = this.models[i].getPredicateEvaluation();
+        }
+        
+        Boolean modelEvaluation = this.modelBooleanEvaluations[0];
+        for (int i = 1; i < this.models.length; i++)
+                modelEvaluation = modelEvaluation && this.modelBooleanEvaluations[i];
 
+        if (modelEvaluation == true && this.query.get)
     }
 
-    public setTruthTable() {
-
+    public String[] getTruthTable() {
+        return this.truthTable;
     }
 
-    public getTruthTable() {
-
-    }
-
-    public getValueTable() {
-
+    public Boolean[] getTableValues() {
+        return this.tableValues;
     }
 
     public Model[] getModels() {
