9e9f0b6f288df4871309734b4c54aa385cc02cae
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-03-22 18:19:38 -0400

    StochasticModel push 1/2
    Started creating StochasticModel class

--- src/LogicType/PropositionalLogic/Models/StochasticModel.java
+++ src/LogicType/PropositionalLogic/Models/StochasticModel.java
@@ -2,92 +2,197 @@ package src.LogicType.PropositionalLogic.Models;
 
 import java.util.Map;
 
+import src.Exceptions.InvalidExpressionException;
+import src.Exceptions.InvalidLogicOperatorException;
+import src.Exceptions.InvalidOperandException;
 import src.LogicType.PropositionalLogic.Logic.Proposition;
+import src.LogicType.PropositionalLogic.Logic.Validity;
 
 public class StochasticModel extends Model {
-    public StochasticModel(String modelName, Map<Character, Double> defaultOperandTruthValues, 
+    private String modelName;
+    private Proposition expression;
+
+    private char[] operands;
+    private Map<Character, Double> defaultOperandTruthValues;
+
+    private double defaultTruthThreshold;
+    private Map<Character, Double> operandTruthThresholds;
+
+    private String predicateModel;
+    private boolean predicateEvaluation;
+    private char[] allPredicateCharValues;
+    private boolean[] totalPredicateBooleanValues;
+    private String validityEvaluation;
+
+    private String symbolRepresentation;
+    private Map<Character, String> operandSymbolicRepresentation;
+
+    public StochasticModel(String modelName, String expression) throws InvalidExpressionException, InvalidOperandException, InvalidLogicOperatorException {
+        if (expression == null || expression.isEmpty()) 
+            throw new IllegalArgumentException("Expression cannot be null or empty.");
+
+        this.modelName = modelName;
+        this.expression = new Proposition(expression);
+    }
+
+    public StochasticModel(String modelName, Proposition expression) {
+        if (expression == null) 
+            throw new IllegalArgumentException("Expression cannot be null or empty.");
+        this.modelName = modelName;
+        this.expression = expression;
+    }
+
+    public StochasticModel(String modelName, String expression, Map<Character, String> operandSymbolicRepresentation) throws InvalidExpressionException, InvalidOperandException, InvalidLogicOperatorException {
+        if (expression == null || expression.isEmpty()) 
+            throw new IllegalArgumentException("Expression cannot be null or empty.");
+        this.modelName = modelName;
+        this.expression = new Proposition(expression);
+
+    }
+
+    public StochasticModel(String modelName, Proposition expression, Map<Character, String> operandSymbolicRepresentation) {
+        if (expression == null) 
+            throw new IllegalArgumentException("Expression cannot be null or empty.");
+        this.modelName = modelName;
+        this.expression = expression;
+    }
+
+    public StochasticModel(String modelName, String expression, Map<Character, String> operandSymbolicRepresentation, Map<Character, Double> defaultOperandTruthValues,  double defaultTruthThreshold) throws InvalidExpressionException, InvalidOperandException, InvalidLogicOperatorException {
+        if (expression == null || expression.isEmpty()) 
+            throw new IllegalArgumentException("Expression cannot be null or empty.");
+        this.modelName = modelName;
+        this.expression = new Proposition(expression);
+    }
+
+    public StochasticModel(String modelName, Proposition expression, Map<Character, String> operandSymbolicRepresentation, Map<Character, Double> defaultOperandTruthValues, double defaultTruthThreshold) {
+        if (expression == null) 
+            throw new IllegalArgumentException("Expression cannot be null or empty.");
+        this.modelName = modelName;
+        this.expression = expression;
+    }
+    
+    public StochasticModel(String modelName, String expression, Map<Character, String> operandSymbolicRepresentation, double defaultTruthThreshold, Map<Character, Double> defaultOperandTruthValues, Map<Character, Double> operandTruthThresholds) throws InvalidExpressionException, InvalidOperandException, InvalidLogicOperatorException {
+        if (expression == null || expression.isEmpty()) 
+            throw new IllegalArgumentException("Expression cannot be null or empty.");
+        this.modelName = modelName;
+        this.expression = new Proposition(expression);
+    }
+
+    public StochasticModel(String modelName, Proposition expression, Map<Character, String> operandSymbolicRepresentation, double defaultTruthThreshold, Map<Character, Double> defaultOperandTruthValues, Map<Character, Double> operandTruthThresholds) {
+        if (expression == null) 
+            throw new IllegalArgumentException("Expression cannot be null or empty.");
+        this.modelName = modelName;
+        this.expression = expression;
+    }
+
+    private void setOperands(Map<Character, Double> defaultOperandTruthValues) {
+        operands = new char[this.expression.getOperandCount()];
+        for (int i = 0; i < this.expression.getOperandCount(); i++) {
+            String operand = this.expression.getSentence(i);
+            operands[i] = operand.charAt(0);
+        }
+        for (int i = 0; i < operands.length; i++) {
+            if (!defaultOperandTruthValues.containsKey(operands[i]))
+                throw new IllegalArgumentException(operands[i] + " not in expression.");
+        }
+    }
+
+    private void setPredicateString(Map<Character, Double> defaultOperandTruthValues, double defaultTruthThreshold, Map<Character, Double> operandTruthThresholds) {
+            
+    }
+
+    private void setAllPredicateTruthValues() {
+
+    }
+
+    private void setValidityEvaluation() {
+        Validity validity = new Validity();
+
+        if (validity.isTautology(this.allPredicateCharValues))
+            this.validityEvaluation = "Tautology";
+        else if (validity.isContradiction(this.allPredicateCharValues))
+            this.validityEvaluation = "Contradiction";
+        else if (validity.isContingency(this.allPredicateCharValues))
+            this.validityEvaluation = "Contingency";
+        else
+            this.validityEvaluation = null;
+    }
+
+    private void setSymbolicString(Map<Character, String> operandSymbolicRepresentation) {
+        
+    }
+
     @Override
     public String getModelName() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getModelName'");
+        return this.modelName;
     }
 
     @Override
     public Proposition getProposition() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getProposition'");
+        return this.expression;
     }
 
     @Override
-    public String[][] getTruthTable() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTruthTable'");
+    public String[][] getTruthTable() throws InvalidExpressionException {
+        return this.expression.getTruthTable();
     }
 
     @Override
     public String getExpression() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getExpression'");
+        return this.expression.getExpression();
     }
 
     @Override
     public char[] getOperands() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOperands'");
+        return this.operands;
     }
 
     @Override
     public char getOperand(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOperand'");
+        if (index < 0 || index >= this.operands.length)
+            throw new IllegalArgumentException("Operand index out of bounds");
+
+        return this.operands[index];
     }
 
-    @Override
-    public Map<Character, Character> getOperandTruthValues() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOperandTruthValues'");
+    
+    public Map<Character, Double> getOperandTruthValues() {
+        return this.defaultOperandTruthValues;
     }
 
     @Override
     public Map<Character, String> getOperandSymbolicRepresentation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOperandSymbolicRepresentation'");
+        return this.operandSymbolicRepresentation;
     }
 
     @Override
     public char[] getAllPredicateTruthValues() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllPredicateTruthValues'");
+        return this.allPredicateCharValues;
     }
 
     @Override
     public boolean[] getAllPredicateBooleanValues() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllPredicateBooleanValues'");
+        return this.totalPredicateBooleanValues;
     }
 
     @Override
     public String getEquivalencyEvaluation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getEquivalencyEvaluation'");
+        return this.equivalencyEvaluation;
     }
 
     @Override
     public String getPredicateModel() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPredicateModel'");
+        return this.predicateModel;
     }
 
     @Override
     public String getSymbolicRepresentation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSymbolicRepresentation'");
+        return this.symbolRepresentation;
     }
 
     @Override
     public Boolean getPredicateEvaluation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPredicateEvaluation'");
+        return this.predicateEvaluation;
     }
     
 }
