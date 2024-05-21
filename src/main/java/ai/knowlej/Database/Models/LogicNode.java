c84c967aecd1a1bf44696a1aa80edc95d96fbd2e
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-05-20 21:12:57 -0400

    LogicNode sentence input validation

--- src/main/java/ai/knowlej/Database/Models/LogicNode.java
+++ src/main/java/ai/knowlej/Database/Models/LogicNode.java
@@ -2,5 +2,18 @@ package ai.knowlej.Database.Models;
 
 import java.util.Map;
 
+import ai.knowlej.PropositionalLogic.Logic.Proposition;
+
 public record LogicNode(String logicSentence, String[] logicLabels, Map<String, String> logicProperties) {
+    public LogicNode {
+        if (logicSentence == null) {
+            throw new IllegalArgumentException("Logic sentence cannot be null");
+        }
+        // test logical Sentence validity
+        try {
+            Proposition propositionTest = new Proposition(logicSentence);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid logic sentence");
+        }
+    }
 }
