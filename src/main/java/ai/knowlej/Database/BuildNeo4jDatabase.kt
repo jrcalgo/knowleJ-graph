4b13f09cb93ab7e4f3230b313588711ad48bf507
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-05-21 17:22:32 -0400

    Update BuildNeo4jDatabase.kt

--- src/main/java/ai/knowlej/Database/BuildNeo4jDatabase.kt
+++ src/main/java/ai/knowlej/Database/BuildNeo4jDatabase.kt
@@ -105,13 +105,16 @@ open class BuildNeo4jDatabase {
     }
 
     open inner class BuildDomainNodes : BuildNeo4jDatabase() {
-        fun checkForDomain(domainName: String?, continueReadSession: Boolean = false): Boolean {
+        fun checkForDomain(domainNode: DomainGroupNode, continueReadSession: Boolean = false): Boolean {
             // check if domain already exists
             var domainExistence: Boolean = false
             try {
                 if (session == null) 
                     openSession('r')
-                val result = session!!.run("MATCH (d:Domain) WHERE d.name = \$domainName RETURN d", mapOf("domainName" to domainName))
+                
+                val parameters = mapOf("domainName" to domainNode.domainName, "labels" to domainNode.domainLabels.joinToString(":"), "domainProperties" to domainNode.domainProperties)
+                val cypher = "MATCH (d:Domain:\$labels {name: \$domainName, properties: \$domainProperties}) WHERE d.name = \$domainName RETURN d"
+                val result = session!!.run(cypher, parameters)
                 if (result.hasNext()) 
                     domainExistence = true      
             } catch (e: Exception) {
@@ -123,18 +126,19 @@ open class BuildNeo4jDatabase {
             return domainExistence
         }
 
-        fun createDomainNode(domainNode: DomainNode) {
-            if (!checkForDomain(domainNode.domainName)) {
-                return
+        fun createDomainNode(domainNode: DomainGroupNode): org.neo4j.driver.types.Node? {
+            if (!checkForDomain(domainNode)) {
+                return null
             }
             // create new domain node
+            var createdNode: org.neo4j.driver.types.Node? = null
             try {
                 openSession('w').use { _ ->
-                    val parameters = mapOf("domainName" to domainNode.domainName, "domainLabels" to domainNode.domainLabels, "domainProperties" to domainNode.domainProperties)
-                    val cypher = "CREATE (d:Domain {name: \$domainName} )"
+                    val parameters = mapOf("domainName" to domainNode.domainName, "labels" to domainNode.domainLabels.joinToString(":"), "domainProperties" to domainNode.domainProperties)
+                    val cypher = "CREATE (d:Domain:\$labels {name: \$domainName, properties: \$domainProperties}) RETURN d"
                     val result = session!!.run(cypher, parameters)
                     if (result.hasNext()) {
-
+                        createdNode = result.next().get("d").asNode()
                     }
                 }
             } catch (e: Exception) {
@@ -142,11 +146,12 @@ open class BuildNeo4jDatabase {
             } finally {
                 closeSession()
             }
+            return createdNode
         }
 
-        fun createDomainRelationship(domainNode1: DomainNode, domainNode2: DomainNode) {
-            if (!checkForDomain(domainNode1.domainName, true) || !checkForDomain(domainNode2.domainName, false)) {
-                return
+        fun createDomainRelationship(domainNode1: DomainGroupNode, domainNode2: DomainGroupNode): org.neo4j.driver.types.Relationship? {
+            if (!checkForDomain(domainNode1, true) || !checkForDomain(domainNode2, false)) {
+                return null
             }
             // check if relationship already exists
             try {
@@ -163,15 +168,15 @@ open class BuildNeo4jDatabase {
     }
 
      open inner class BuildSubdomainNodes : BuildDomainNodes() {
-         fun checkForSubdomain(domainName: String, subdomainName: String, continueReadSession: Boolean = false): Boolean {
+         fun checkForSubdomain(domainNode: DomainGroupNode, subdomainNode: SubdomainGroupNode, continueReadSession: Boolean = false): Boolean {
             //  check if subdomain and parent domain already exists
             var subdomainExistence: Boolean = false
             try {
-                if (checkForDomain(domainName, true)) {
-                    val result = session!!.run(
-                        "MATCH (d:Domain)-[:DOMAIN_OF]-(s:Subdomain) WHERE d.name = \$domainName AND s.name = \$subdomainName RETURN s",
-                        mapOf("domainName" to domainName, "subdomainName" to subdomainName)
-                    )
+                if (checkForDomain(domainNode, true)) {
+                    val domainParameters = mapOf("domainName" to domainNode.domainName, "domainLabels" to if (domainNode.domainLabels.isNotEmpty()) domainNode.domainLabels.joinToString(":") else "", "domainProperties" to if (domainNode.domainProperties.isNotEmpty()) domainNode.domainProperties else "")
+                    val subdomainParameters = mapOf("subdomainName" to subdomainNode.subdomainName, "subdomainLabels" to subdomainNode.subdomainLabels.joinToString(":"), "subdomainProperties" to subdomainNode.subdomainProperties)
+                    val cypher = "MATCH (d:Domain:\$domainLabels {name: \$domainName, properties: \$domainProperties})-[:DOMAIN_OF]-(s:Subdomain:\$subdomainLabels {name: \$subdomainName, properties: \$subdomainProperties}) RETURN s"
+                    val result = session!!.run(cypher, domainParameters+subdomainParameters)
                     if (result.hasNext())
                         subdomainExistence = true
                 }
@@ -184,38 +189,44 @@ open class BuildNeo4jDatabase {
             return subdomainExistence
          }
 
-        fun createSubdomainNode(domainName: String, sbdNode: SubdomainNode): Boolean? {
+        fun createSubdomainNode(domainNode: DomainGroupNode, subdomainNode: SubdomainGroupNode): org.neo4j.driver.types.Node? {
             // check if subdomain already exists
-            if (!checkForSubdomain(domainName, sbdNode.subdomainName)) {
+            if (!checkForSubdomain(domainNode, subdomainNode)) {
                 return null
             }
             // check and create new subdomain node
-            var writeSuccessful = false
+            var createdNode: org.neo4j.driver.types.Node? = null
             try {
                 openSession('w').use { _ ->
-                    val parameters = mapOf("domainName" to domainName, "subdomainName" to sbdNode.subdomainName, "subdomainLabels" to sbdNode.subdomainLabels, "subdomainProperties" to sbdNode.subdomainProperties)
-                    val cypher = "MERGE (d:Domain {name})-[:DOMAIN_OF]-(s:Subdomain: \$subdomainLabels {name: \$subdomainName} ON CREATE SET s += \$subdomainProperties) ON MATCH RETURN s"
+                    val domainParameters = mapOf("domainName" to domainNode.domainName, "domainLabels" to domainNode.domainLabels.joinToString(":"), "domainProperties" to domainNode.domainProperties)
+                    val subdomainParameters = mapOf("subdomainName" to subdomainNode.subdomainName, "subdomainLabels" to subdomainNode.subdomainLabels.joinToString(":"), "subdomainProperties" to subdomainNode.subdomainProperties)
+                    val parameters = domainParameters+subdomainParameters
+                    val cypher = "MERGE (d:Domain {name})-[:DOMAIN_OF]-(s:Subdomain: \$subdomainLabels {name: \$subdomainName, properties: \$subdomainProperties} RETURN s"
                     val result = session!!.run(cypher, parameters)
                     if (result.hasNext())
-                        writeSuccessful = true
+                        createdNode = result.next().get("s").asNode()
                 }
             } catch (e: Exception) {
                 throw Exception("createSubdomainNode failed!")
             } finally {
                 closeSession()
             }
-            return writeSuccessful
+            return createdNode
         }
 
-        fun createSubdomainRelationship(domainFromName: String, subdomainFromName: String, domainToName: String, subdomainToName: String, relationshipLabels: Array<String>?, relationshipProperties: HashMap<String, String>?): Boolean? {
-            if (!checkForSubdomain(domainFromName, subdomainFromName, true) || !checkForSubdomain(domainToName, subdomainToName, false)) {
+        fun createSubdomainRelationship(domainNode1: DomainGroupNode, subdomainNode1: SubdomainGroupNode, domainNode2: DomainGroupNode, subdomainNode2: SubdomainGroupNode, relationshipLabels: Array<String>?, relationshipProperties: HashMap<String, String>?): Boolean? {
+            if (!checkForSubdomain(domainNode1, subdomainNode1, true) || !checkForSubdomain(domainNode2, subdomainNode2, false)) {
                 return null
             }
             // check and create new subdomain relationship
             var relationshipExistence = false
             try {
                 openSession('w').use { _ ->
-                    val parameters = mapOf("domainFromName" to domainFromName, "subdomainFromName" to subdomainFromName, "domainToName" to domainToName, "subdomainToName" to subdomainToName)
+                    val fromDomainParameters = mapOf("fromDomainName" to domainNode1.domainName, "fromDomainLabels" to domainNode1.domainLabels.joinToString(":"), "fromDomainProperties" to domainNode1.domainProperties)
+                    val fromSubdomainParameters = mapOf("fromSubdomainName" to subdomainNode1.subdomainName, "fromSubdomainLabels" to subdomainNode1.subdomainLabels.joinToString(":"), "fromSubdomainProperties" to subdomainNode1.subdomainProperties)
+                    val toDomainParameters = mapOf("toDomainName" to domainNode2.domainName, "toDomainLabels" to domainNode2.domainLabels.joinToString(":"), "toDomainProperties" to domainNode2.domainProperties)
+                    val toSubdomainParameters = mapOf("toSubdomainName" to subdomainNode2.subdomainName, "toSubdomainLabels" to subdomainNode2.subdomainLabels.joinToString(":"), "toSubdomainProperties" to subdomainNode2.subdomainProperties)
+                    val parameters = fromDomainParameters+fromSubdomainParameters+toDomainParameters+toSubdomainParameters
                     val cypher = "MERGE (d1:Domain {name: \$domainFromName})-[:DOMAIN_OF]->(s1:Subdomain {name: \$subdomainFromName})-(s2:Subdomain {name: \$subdomainToName})<-[:DOMAIN_OF]-(d2:Domain {name: \$domainToName})"
                     val result = session!!.run(cypher, parameters)
                     if (result.hasNext()) {
@@ -232,14 +243,15 @@ open class BuildNeo4jDatabase {
     }
 
     open inner class BuildKnowledgeBaseNodes : BuildSubdomainNodes() {
-        fun createAbstractKB(domainName: String, subdomainName: String, abstractNodes: ArrayList<String>) {
-            if (!checkForSubdomain(domainName, subdomainName)) {
-                return
+        fun createAbstractKB(domainNode: DomainGroupNode, subdomainNode: SubdomainGroupNode, abstractKBNode: AbstractKBNode): org.neo4j.driver.types.Node? {
+            if (!checkForSubdomain(domainNode, subdomainNode)) {
+                return null
             }
             // check and create knowledge base nodes
             try {
                 openSession('w').use { _ ->
-                    
+                    val parameters = mapOf("")
+                    var cypher = 
                 }
             } catch (e: Exception) {
 
@@ -248,25 +260,34 @@ open class BuildNeo4jDatabase {
             }
         }
 
-        fun createLogicalKB(domainName: String, subdomainName: String, premiseNodes: Array<LogicNode>) {
-            if (!checkForSubdomain(domainName, subdomainName, true)) {
-                return
+        fun createLogicalKB(domainNode: DomainGroupNode, subdomainNode: SubdomainGroupNode, logicKBNode: LogicKBNode): org.neo4j.driver.types.Node? {
+            if (!checkForSubdomain(domainNode, subdomainNode)) {
+                return null
             }
-
+            var createdKB: org.neo4j.driver.types.Node? = null
             try {
                 openSession('w').use { _ ->
+                    val parameters = mapOf("domainName" to domainName, "subdomainName" to subdomainName, "logicGroupName" to logicKBNode.logicGroupName, "logicGroupLabels" to logicKBNode.logicGroupLabels, "logicGroupProperties" to logicKBNode.logicGroupProperties)
+                    var cypher = "MERGE (d:Domain {name:})"
+                    for (node in logicKBNode.logicNodes) {
+                        val nodeParameters = mapOf("logicSentence" to node.logicSentence, "nodeLabels" to node.logicLabels, "nodeProperties" to node.logicProperties)
+                        cypher = 
+                        val result
+                        if (result.hasNext()) {
 
+                        }
+                    }
                 }
             } catch (e: Exception) {
 
             } finally {
                 closeSession()
             }
-            
+            return createdKB
         }
 
-        private fun createKBNode(domainName: String, subdomainName: String, lN: LogicNode): Boolean? {
-            if (!checkForSubdomain(domainName, subdomainName)) {
+        private fun createAbstractNode(domainNode: DomainGroupNode, subdomainNode: SubdomainGroupNode,  abstractNode: AbstractKnowledgeNode): org.neo4j.driver.types.Node? {
+            if (!checkForSubdomain(domainNode, subdomainNode)) {
                 return null
             }
             // check and create knowledge base node
@@ -279,17 +300,21 @@ open class BuildNeo4jDatabase {
             } finally {
                 closeSession()
             }
+            return null
         }
 
-        fun createKBNodeRelationship(fromNode: String, toNode: String, relationshipLabels: Array<String>?, relationshipProperties: HashMap<String, String>?) {
+        fun createKBNodeRelationship(fromNode: Any, toNode: Any, relationshipLabels: Array<String>?, relationshipProperties: HashMap<String, String>?): org.neo4j.driver.types.Relationship? {
+            if (!(fromNode is LogicNode) && !(fromNode is AbstractKnowledgeNode)) {
+                return null
+            } else if (!(toNode is LogicNode) && !(toNode is AbstractKnowledgeNode)) {
+                return null
+            }
 
+            return null
         }
 
-        fun mergeInferenceChain(
-            iKBName: String?,
-            graph: DirectedDeductionGraph?,
-            inferenceChain: ArrayList<DeductionGraphNode?>?
-        ) {
+        fun mergeInferenceChain(logicGroup: LogicGroupNode, graph: DirectedDeductionGraph?, inferenceChain: ArrayList<DeductionGraphNode?>?) {
+
         }
     }
 
