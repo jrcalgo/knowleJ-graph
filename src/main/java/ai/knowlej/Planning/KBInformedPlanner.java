69fa2ae40dcda6b6385ad6c112def57c3e80c195
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-06-01 15:15:53 -0400

    KBInformedPlanner base class done
    global variables, setters, getters

--- src/main/java/ai/knowlej/Planning/KBInformedPlanner.java
+++ src/main/java/ai/knowlej/Planning/KBInformedPlanner.java
@@ -1,4 +1,36 @@
 package ai.knowlej.Planning;
 
+import ai.knowlej.DataStructures.ActionDomain;
+import ai.knowlej.DataStructures.StateDomain;
+
+import java.util.ArrayList;
+
 public class KBInformedPlanner {
+    private ActionDomain actions;
+    private StateDomain currentState;
+    private StateDomain goalState;
+
+    private ArrayList<String[]> actionHistory;
+    private ArrayList<String[]> stateHistory;
+
+    public KBInformedPlanner(StateDomain goalState) {
+        // actions and currentState are pulled from neo4j database KBs
+        this.goalState = goalState;
+    }
+
+    public void setGoalState(StateDomain goalState) {
+        this.goalState = goalState;
+    }
+
+    public StateDomain getGoalStateDomain() {
+        return this.goalState;
+    }
+
+    public StateDomain getCurrentStateDomain() {
+        return this.currentState;
+    }
+
+    public ActionDomain getActionDomain() {
+        return this.actions;
+    }
 }
